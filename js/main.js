0c6177a899bd0f052fb47543b35dc9d9b3c01eb5
Jorge Valencia <info@jorgevalencia.co>
2016-05-02 15:10:59 +0200

    Added files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+    // scroll animation
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+    // menu animation
+    $( ".menu-mobile-btn" ).on( "click", function() {
+      $(this).toggleClass( "closed" );
+      $('.menu').toggleClass( "closed" );
+    });
+
+    // content animation
+    $('.js-show').addClass("hidden").viewportChecker({
+	    classToAdd: 'visible animated fadeInDown', // Class to add to the elements when they are visible
+	    offset: 100
+	   });
+
+    // menu mobile
+    if ($(window).width() < 768) {
+       $('.menu').addClass('closed');
+    } else {
+       $('.menu').removeClass('closed');
+    }
+
+});
